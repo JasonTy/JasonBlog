bcc63f942ad897e58fe9031a70ac0679397dab58
JasonTy <1026443062@qq.com>
2018-05-21 18:52:48 +0800

    add middleware

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const Koa = require('koa');
 const compress = require('koa-compress');
+const responseTime = require('koa-response-time');
 const router = require('./router/index');
 const app = new Koa();
 
 app
+    .use(compress())
+    .use(responseTime())
     .use(router.routes())
-    .use(router.allowedMethods())
-    .use(compress());
+    .use(router.allowedMethods());
+
 
 app.listen(8008);
