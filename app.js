69cea90fd59fefe29a0eb0cb5606a0859e4986ae
唐轶 <tangyi057@1caifu.com>
2018-05-21 18:23:39 +0800

    update

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const Koa = require('koa');
+const compress = require('koa-compress');
 const router = require('./router/index');
 const app = new Koa();
 
 app
     .use(router.routes())
-    .use(router.allowedMethods());
+    .use(router.allowedMethods())
+    .use(compress());
 
 app.listen(8008);
